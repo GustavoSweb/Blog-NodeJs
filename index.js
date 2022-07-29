1caf1347a3d06265abb0b7e56ceac7248726f9ba
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2022-07-28 21:27:00 -0300

    eita

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
   const app = express()
   const bodyParser = require("body-parser")
   const admin = require("./routers/admin.js")
+  const path = require("path")
+  const PORT = 8081
 // Configurações
 
   // Handlebars
@@ -13,17 +15,26 @@
   // body parser
      app.use(bodyParser.urlencoded({ extended: false }))
      app.use(bodyParser.json())
-  
-// Rotas
+  // Mongosse //
+  // Boostrap //
+    app.use(express.static("public"))
+
+
+// Rotas //
   app.get("/", (req, res)=>{
-    res.send("essa e a pagina principal")
+    res.render("admin/index")
   })
   app.get("/post", (req, res)=>{
     res.send("Lista de postagens: ")
   })
+  app.get("/admin/categorias", (req, res)=>{
+    res.render("/admin/categorias")
+  })
+  app.get("/admin/categorias/add", (req, res)=> {
+    res.render("/admin/categoriasadd")
+  })
   app.use("/admin", admin)
-// Outros
-  const PORT = 8081
+// Outros //
   app.listen(PORT, ()=> {
     console.log("Servidor Rodando na porta: "+PORT)
   })
